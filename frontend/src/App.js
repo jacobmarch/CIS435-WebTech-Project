a080bc2e7119a2912476489a2d43c1feb08c0f39
Jacob Marchywka <dcfalcons2021@gmail.com>
2023-11-02 15:34:19 -0400

    Added routing, still needs bug fixing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,43 @@
 import Header from './components/Header';
 import './App.css';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Account from "./pages/Account";
+import MainFeed from "./pages/MainFeed";
+import Search from "./pages/Search";
+import Trending from "./pages/Trending";
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+
+    //Initialize router
+    const router = createBrowserRouter([
+        {
+            element: <Header />,
+            children: [
+                {
+                    path: "/",
+                    element: <MainFeed/>
+                },
+                {
+                    path: "/trending",
+                    element: <Trending/>
+                },
+                {
+                    path: "/account",
+                    element: <Account/>
+                },
+                {
+                    path: "/search",
+                    element: <Search/>
+                },
+            ],
+        },
+    ])
+
+    return (
+        <div className="App">
+            <RouterProvider router={router} />
+        </div>
+    );
 }
 
 export default App;
