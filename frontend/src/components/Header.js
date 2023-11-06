168eb525edcbef95f1e894b49a92946f07a85271
Jacob Marchywka <32488828+jacobmarch@users.noreply.github.com>
2023-11-06 15:12:56 -0500

    Minor reformatting

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -19,7 +19,7 @@ const Header = () => {
       
       {/* Left section: Logo and Title */}
       <div style={{ display: 'flex', alignItems: 'center' }}>
-        <img src="/DearbornVoice.png" alt="Logo" style={{ marginRight: '10px', height: '40px', width: '120px' }} />
+        <img src="/DearbornVoice.png" alt="Logo" style={{ marginRight: '10px', height: '40px', width: '70px' }} />
       </div>
 
 
