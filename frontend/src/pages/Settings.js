c8fd5ed458b19a0ee69e8f4b455db857bc1493af
Jacob Marchywka <32488828+jacobmarch@users.noreply.github.com>
2023-11-07 15:19:42 -0500

    Made page unable to be scrolled on

--- frontend/src/pages/Settings.js
+++ frontend/src/pages/Settings.js
@@ -13,6 +13,15 @@ const SettingsPage = () => {
     setActiveSection(section);
   };
 
+  React.useEffect(() => {
+    const originalStyle = window.getComputedStyle(document.body).overflow;
+    document.body.style.overflow = 'hidden';
+
+    return () => {
+        document.body.style.overflow = originalStyle;
+    }
+}, []);
+
   const renderActiveSection = () => {
     switch (activeSection) {
       case 'View Profile':
